41e76dda3e61c4db1b2670d1d54769651603a36e
abdulsattar0617 <119532391+abdulsattar0617@users.noreply.github.com>
2023-06-08 10:36:06 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask 
+from flask import Flask , render_template
+import json 
+   
+  
+with open("templates/config.json", 'r') as config:   
+  params = json.load(config)["params"] 
+  
+  app = Flask(__name__) 
+   
+  @app.route("/")
+  def hello_world(): 
+    return render_template('home.html', params=params)   
 
-app = Flask(__name__) 
+ 
 
-@app.route("/")
-def hello_world():
-  return "Hello, World !" 
+
+
+
+
 
 
 if __name__ == "__main__":
